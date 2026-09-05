062eea808b913794724c48dedb2b98d23e94c5b2
tux4us <tux4us@online.de>
2026-09-05 15:57:12 +0200

    VollaHub_6.4 Version: 23
    - Designoptimierung

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.volla.hub"
         minSdk = 24
         targetSdk = 36
-        versionCode = 22
-        versionName = "6.3"
+        versionCode = 23
+        versionName = "6.4"
     }
 
     signingConfigs {
